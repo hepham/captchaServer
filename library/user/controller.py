593a505236e7fdb7d1581da97a71bbd17b725f09
hepham <phamvanhed2lequydon@gmail.com>
2022-03-18 15:45:36 +0700

    update

--- library/user/controller.py
+++ library/user/controller.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_cors import cross_origin
 
 from .services import (get_infor_by_id_service, sign_up_service, get_all_service, change_key, update_captcha_number,
-                       sign_in)
+                       sign_in,predict)
 
 users = Blueprint("users", __name__)
 
@@ -30,17 +30,21 @@ def get_user_by_id(id):
 
 
 # change key
-@users.route("/users/changekey/<int:id>", methods=['GET'])
-def update_book_by_id(id):
-    return change_key(id)
+@users.route("/users/changekey", methods=['POST'])
+def update_captcha_by_merchant_key():
+    return change_key()
 
 
-@users.route("/users/captcha/<int:id>", methods=['PUT'])
-def update_capcha_by_id(id):
-    return update_captcha_number(id)
+@users.route("/users/captcha/updateNumber", methods=['POST'])
+def update_capcha_by_id():
+    return update_captcha_number()
 
 
 @users.route("/users/signin", methods=['POST'])
 @cross_origin()
 def signin():
     return sign_in()
+@users.route('/detect',methods=['POST'])
+@cross_origin()
+def encode():
+    return predict()
--- library/user/services.py
+++ library/user/services.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 
-from flask import request, jsonify
+from flask import  jsonify
 
 from library.encrypt import random_string, encrypt
 from library.extension import db
 from library.library_ma import UserSchema
 from library.model import Users
-
+import base64
+from PIL import Image
+import io
+from flask import request
+# PyTorch Hub
+import numpy as np
+import cv2
+import torch
+model = torch.hub.load('ultralytics/yolov5', 'custom', 'model.pt')
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
@@ -79,41 +87,214 @@ def get_all_service():
 
 # change merchant key
 
-def change_key(id):
-    user = Users.query.get(id)
-    if user:
-        try:
-            now = datetime.now()
-            print(user.email)
-            temp = user.email + str(now)
-            user.merchant_key = encrypt(temp) + random_string(8, 4)
-            db.session.commit()
-            return user_schema.jsonify(user)
-        except IndentationError:
-            db.session.rollback()
-            return jsonify({"message": "Something wrong!"}), 200
-    else:
-        return "not found user"
+def change_key():
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        user = Users.query.filter_by(merchant_key=data['merchant_key']).first()
+        # print(user.password)
+        if user:
+            try:
+                now = datetime.now()
+                print(user.email)
+                temp = user.email + str(now)
+                user.merchant_key = encrypt(temp) + random_string(8, 4)
+                db.session.commit()
+                print(user_schema.jsonify(user).data)
+                return jsonify({'message': 'success', 'email': user.email, 'merchant_key': user.merchant_key,
+                            'count_captcha': user.count_captcha}), 200
+            except IndentationError:
+                db.session.rollback()
+                return jsonify({"message": "Something wrong!"}), 200
+        else:
+            return "not found user"
 
 
 # update number captcha
 
-def update_captcha_number(email):
-    user = Users.query.get(email)
-    data = request.json
+def update_captcha_number():
+    if request.method == 'POST':
+        data = request.json
+        user = Users.query.filter_by(email=data['email']).first()
+        if user:
+            if data and "count_captcha" in data:
+                try:
+                    user.count_captcha = user.count_captcha + int(data["count_captcha"])
+                    db.session.commit()
+                    now = datetime.now()
+                    return jsonify({
+                        "email": user.email,
+                        "count_capcha": user.count_captcha,
+                        "time": str(now)
+                    })
+                except IndentationError:
+                    db.session.rollback()
+                    return jsonify({"message": "something is wrong"}), 200
+        else:
+            return jsonify({"message": "not found"}), 200
+
+def chuyen_base64_sang_anh(anh_base64):
+    try:
+        anh_base64 = np.fromstring(base64.b64decode(anh_base64), dtype=np.uint8)
+        anh_base64 = cv2.imdecode(anh_base64, cv2.IMREAD_ANYCOLOR)
+    except:
+        return None
+    return anh_base64
+# @app.route('/',methods=['GET'])
+# def hello_world():  # put application's code here
+#     return render_template('index.html')
+
+def predict():
+    # imagefile= request.files['imagefile']
+    # image_path="./images/"+imagefile.filename
+    # imagefile.save(image_path)
+    # print(request.form.get('captcha'))
+    captchaImage=request.form.get('captcha')
+    key=request.form.get('merchant_key')
+    user = Users.query.filter_by(merchant_key=key).first()
+    # print(user.password)
     if user:
-        if data and "count_captcha" in data:
-            try:
-                user.count_captcha = user.count_captcha + int(data["count_captcha"])
-                db.session.commit()
-                now = datetime.now()
-                return jsonify({
-                    "email": user.email,
-                    "count_capcha": user.count_captcha,
-                    "time": str(now)
-                })
-            except IndentationError:
-                db.session.rollback()
-                return jsonify({"message": "something is wrong"}), 200
-    else:
-        return jsonify({"message": "not found"}), 200
+        if user.count_captcha>0:
+            user.count_captcha=user.count_captcha-1;
+            db.session.commit()
+        # imagetemp=chuyen_base64_sang_anh(capchaImage)
+        # image=cv2.cvtColor(imagetemp,cv2.COLOR_BGR2RGB)
+            imagedata = base64.b64decode(captchaImage)
+            buf = io.BytesIO(imagedata)
+            image=Image.open(buf)
+            image = image.resize((640, 640))
+            print(image.size)
+            # image=image.resize(640,640)
+            # cv2.imshow('image', image)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            results = model(image)
+            results.print()  # or .show(), .save()
+            results.xyxy[0]  # im predictions (tensor)
+            # print(results.pandas().xyxy[0])  # im predictions (pandas)
+            xmin = 0;
+            ymin = 10;
+            ymax = 200;
+            count = 0;
+            array = []
+            for i in range(len(results.pandas().xyxy[0].name)):
+                if results.pandas().xyxy[0].ymin[i] < ymax:
+                    array.append(results.pandas().xyxy[0].xmin[i])
+            str = ''
+            array.sort()
+            for i in range(len(array)):
+                for j in range(len(results.pandas().xyxy[0].name)):
+                    if array[i] == results.pandas().xyxy[0].xmin[j]:
+                        if (results.pandas().xyxy[0].name[j] == 'a1'):
+                            results.pandas().xyxy[0].name[j] = '&'
+                            str = str + '&'
+                        elif (results.pandas().xyxy[0].name[j] == 'a2'):
+                            results.pandas().xyxy[0].name[j] = '@'
+                            str = str + '@'
+                        elif (results.pandas().xyxy[0].name[j] == 'a3'):
+                            results.pandas().xyxy[0].name[j] = '#'
+                            str = str + '#'
+                        elif (results.pandas().xyxy[0].name[j] == 'a4'):
+                            results.pandas().xyxy[0].name[j] = '%'
+                            str = str + '%'
+                        elif (results.pandas().xyxy[0].name[j] == 'a5'):
+                            results.pandas().xyxy[0].name[j] = '$'
+                            str = str + '$'
+                        else:
+                            str = str + results.pandas().xyxy[0].name[j]
+            # print(str)
+            strcompare = ''
+            ymax = 360
+            array = []
+            for i in range(len(results.pandas().xyxy[0].name)):
+                if results.pandas().xyxy[0].ymin[i] < ymax and results.pandas().xyxy[0].ymin[i] > 250:
+                    array.append(results.pandas().xyxy[0].xmin[i])
+            array.sort()
+            for i in range(len(array)):
+                for j in range(len(results.pandas().xyxy[0].name)):
+                    if array[i] == results.pandas().xyxy[0].xmin[j]:
+                        if (results.pandas().xyxy[0].name[j] == 'a1'):
+                            results.pandas().xyxy[0].name[j] = '&'
+                            strcompare = strcompare + '&'
+                        elif (results.pandas().xyxy[0].name[j] == 'a2'):
+                            results.pandas().xyxy[0].name[j] = '@'
+                            strcompare = strcompare + '@'
+                        elif (results.pandas().xyxy[0].name[j] == 'a3'):
+                            results.pandas().xyxy[0].name[j] = '#'
+                            strcompare = strcompare + '#'
+                        elif (results.pandas().xyxy[0].name[j] == 'a4'):
+                            results.pandas().xyxy[0].name[j] = '%'
+                            strcompare = strcompare + '%'
+                        elif (results.pandas().xyxy[0].name[j] == 'a5'):
+                            results.pandas().xyxy[0].name[j] = '$'
+                            strcompare = strcompare + '$'
+                        else:
+                            strcompare = strcompare + results.pandas().xyxy[0].name[j]
+
+            ymax = 450
+            array = []
+            for i in range(len(results.pandas().xyxy[0].name)):
+                if results.pandas().xyxy[0].ymin[i] < ymax and results.pandas().xyxy[0].ymin[i] > 370:
+                    array.append(results.pandas().xyxy[0].xmin[i])
+            array.sort()
+            for i in range(len(array)):
+                for j in range(len(results.pandas().xyxy[0].name)):
+                    if array[i] == results.pandas().xyxy[0].xmin[j]:
+                        if (results.pandas().xyxy[0].name[j] == 'a1'):
+                            results.pandas().xyxy[0].name[j] = '&'
+                            strcompare = strcompare + '&'
+                        elif (results.pandas().xyxy[0].name[j] == 'a2'):
+                            results.pandas().xyxy[0].name[j] = '@'
+                            strcompare = strcompare + '@'
+                        elif (results.pandas().xyxy[0].name[j] == 'a3'):
+                            results.pandas().xyxy[0].name[j] = '#'
+                            strcompare = strcompare + '#'
+                        elif (results.pandas().xyxy[0].name[j] == 'a4'):
+                            results.pandas().xyxy[0].name[j] = '%'
+                            strcompare = strcompare + '%'
+                        elif (results.pandas().xyxy[0].name[j] == 'a5'):
+                            results.pandas().xyxy[0].name[j] = '$'
+                            strcompare = strcompare + '$'
+                        else:
+                            strcompare = strcompare + results.pandas().xyxy[0].name[j]
+            ymax = 530
+            array = []
+            for i in range(len(results.pandas().xyxy[0].name)):
+                if results.pandas().xyxy[0].ymin[i] < ymax and results.pandas().xyxy[0].ymin[i] > 460:
+                    array.append(results.pandas().xyxy[0].xmin[i])
+            array.sort()
+            for i in range(len(array)):
+                for j in range(len(results.pandas().xyxy[0].name)):
+                    if array[i] == results.pandas().xyxy[0].xmin[j]:
+                        if (results.pandas().xyxy[0].name[j] == 'a1'):
+                            results.pandas().xyxy[0].name[j] = '&'
+                            strcompare = strcompare + '&'
+                        elif (results.pandas().xyxy[0].name[j] == 'a2'):
+                            results.pandas().xyxy[0].name[j] = '@'
+                            strcompare = strcompare + '@'
+                        elif (results.pandas().xyxy[0].name[j] == 'a3'):
+                            results.pandas().xyxy[0].name[j] = '#'
+                            strcompare = strcompare + '#'
+                        elif (results.pandas().xyxy[0].name[j] == 'a4'):
+                            results.pandas().xyxy[0].name[j] = '%'
+                            strcompare = strcompare + '%'
+                        elif (results.pandas().xyxy[0].name[j] == 'a5'):
+                            results.pandas().xyxy[0].name[j] = '$'
+                            strcompare = strcompare + '$'
+                        else:
+                            strcompare = strcompare + results.pandas().xyxy[0].name[j]
+            # print(strcompare)
+            i = 0
+
+            while i < len(strcompare) - 1:
+                if (strcompare[i + 1].isdigit()):
+                    str = str.replace(strcompare[i], strcompare[i + 1])
+                    i = i + 1
+                else:
+                    i = i + 1
+
+            result = ''.join([i for i in str if i.isdigit()])
+            print(result)
+            return result
+        return jsonify({"message": "your captcha is out"}), 200
+    return jsonify({"message": "can't found user"}), 200
